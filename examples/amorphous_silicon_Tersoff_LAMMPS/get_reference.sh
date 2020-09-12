26a7bce9cf8a5137358cc26c6fd73726b18e537a
zkunchen <zkuchen@ucdavis.edu>
2020-09-12 11:04:48 -0700

    Update get_reference.sh

--- examples/amorphous_silicon_Tersoff_LAMMPS/get_reference.sh
+++ examples/amorphous_silicon_Tersoff_LAMMPS/get_reference.sh
@@ -1,6 +1,7 @@
 # get_reference.sh is used to 
 # obtain reference calculations
-#  for example 2
+#  for example amorphous_silicon_Tersoff_LAMMPS
+
 
 # Fetch precalculated forceconstant folder from remote
 wget http://sophe.ucdavis.edu/structures/reference/updated_example/amorphous_silicon_Tersoff_LAMMPS.tar.gz
