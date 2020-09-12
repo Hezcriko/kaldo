0949bd7d49373115943a36b3be0e106168cf4480
zkunchen <zkuchen@ucdavis.edu>
2020-09-12 10:25:32 -0700

    Update get_reference.sh

--- examples/carbon_diamond_Tersoff_ASE_LAMMPS/get_reference.sh
+++ examples/carbon_diamond_Tersoff_ASE_LAMMPS/get_reference.sh
@@ -3,11 +3,11 @@
 #  for example 1
 
 # Fetch precalculated force constants folder from remote
-wget http://sophe.ucdavis.edu/structures/reference/reference_1_carbon_diamond_Tersoff_ASE_LAMMPS.tar.gz
+wget http://sophe.ucdavis.edu/structures/reference/carbon_diamond_Tersoff_ASE_LAMMPS.tar.gz
 
 # Untar precalculated files and clean up 
-tar xzvf reference_1_carbon_diamond_Tersoff_ASE_LAMMPS.tar.gz
-rm -rf reference_1_carbon_diamond_Tersoff_ASE_LAMMPS.tar.gz
+tar xzvf carbon_diamond_Tersoff_ASE_LAMMPS.tar.gz
+rm -rf carbon_diamond_Tersoff_ASE_LAMMPS.tar.gz
 
 echo  "  "
 echo "Reference calculation files are obtained."
